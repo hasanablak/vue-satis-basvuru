eb1714c2899b816dc8b46cd0ce8c5fb7e346b366
hasanablak <0hasanablak@gmail.com>
2023-01-12 21:14:55 +0300

    dropdown menu nested

---
+++ src/components/DopdownMenu/MenuDropdownAdmin.vue
@@ -0,0 +1,82 @@
+<template>
+	<div class="dropdown" :class="isDropdownOpen && 'show'">
+		<button class="btn dropdown-toggle btn text-white" @click="isDropdownOpen = !isDropdownOpen" type="button"
+			id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
+			Admin
+		</button>
+		<ul class="dropdown-menu" :class="isDropdownOpen && 'show'" aria-labelledby="dropdownMenuButton">
+			<li>
+				<RouterLink class="dropdown-item" to="/">A1. Parametreler</RouterLink>
+			</li>
+			<li>
+				<RouterLink class="dropdown-item" to="/">A2. Üyelik Ücretlendirme</RouterLink>
+			</li>
+			<li>
+				<RouterLink class="dropdown-item" to="/">A3. Kampanyalar</RouterLink>
+			</li>
+			<li>
+				<a class="dropdown-item" href="#">
+					A4. Tanımlar &raquo;
+				</a>
+				<ul class="dropdown-menu dropdown-submenu">
+					<li>
+						<a class="dropdown-item" href="#">A4.1* Genel Tanımlar &raquo;</a>
+						<ul class="dropdown-menu dropdown-submenu">
+							<li>
+								<a class="dropdown-item" href="#">A4.1.1* Grup Kodu ve Adı Tanımları &raquo;</a>
+								<ul class="dropdown-menu dropdown-submenu">
+									<li>
+										<a class="dropdown-item" href="#">
+											<button class="btn btn-success">Yeni +</button>
+										</a>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a class="dropdown-item" href="#">A4.1.2* Modül Kodu ve Adı Tanımları</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a class="dropdown-item" href="#">A4.1* Modül Tanımları</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<RouterLink class="dropdown-item" to="/">A5. Metinler</RouterLink>
+			</li>
+			<li>
+				<RouterLink class="dropdown-item" to="/">A6. Opsiyonlar</RouterLink>
+			</li>
+			<li>
+				<RouterLink class="dropdown-item" to="/admin/color-options">A7. Renk Seçenekleri</RouterLink>
+			</li>
+		</ul>
+	</div>
+</template>
+<script setup>
+import { ref } from "vue";
+import Tanimlar from "./Tanimlar.vue";
+const isDropdownOpen = ref(false);
+</script>
+<style scoped>
+.dropdown-menu li {
+	position: relative;
+}
+
+.dropdown-menu .dropdown-submenu {
+	display: none;
+	position: absolute;
+	left: 100%;
+	top: -7px;
+}
+
+.dropdown-menu .dropdown-submenu-left {
+	right: 100%;
+	left: auto;
+}
+
+.dropdown-menu>li:hover>.dropdown-submenu {
+	display: block;
+}
+</style>

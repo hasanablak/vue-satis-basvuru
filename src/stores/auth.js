9a7a92b8e6d22b364bb3d22306dc51ebfa426356
Hasan Ablak <0hasanablak@gmail.com>
2023-01-13 10:52:30 +0300

    auth tipi admin veya user olarabilir

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -4,14 +4,11 @@ import { defineStore } from "pinia";
 export const useAuthStore = defineStore({
 	id: 'auth',
 	state: () => ({
-		user: {}
+		type: 'user'
 	}),
 	actions: {
-		removeItemsToBasket(item) {
-			let itemIndex_ = this.items.findIndex(s => s.id == item.id);
-			if (itemIndex_ != -1) {
-				this.items.splice(itemIndex_, 1);
-			}
+		setType(type) {
+			this.type = type;
 		}
 	},
 	getters: {

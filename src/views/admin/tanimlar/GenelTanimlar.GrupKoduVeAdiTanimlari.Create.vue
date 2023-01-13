421eacd748773640c8381ab083d4ff56c31b2f14
Hasan Ablak <0hasanablak@gmail.com>
2023-01-13 11:03:21 +0300

    Admin group tanımlarının create ve list sayfalarını oluşturdum

---
+++ src/views/admin/tanimlar/GenelTanimlar.GrupKoduVeAdiTanimlari.Create.vue
@@ -0,0 +1,87 @@
+<template>
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<div class="card-header">
+					<div class="row">
+						<div class="col">
+							Grup Kodu ve Adı Tanımlama
+						</div>
+					</div>
+				</div>
+				<div class="card-body">
+					<form @submit.prevent="submit()">
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text" id="basic-addon1">{{
+									getGroupCode()
+								}}</span>
+							</div>
+							<input v-model="name" type="text" class="form-control" placeholder="Grup adı giriniz">
+						</div>
+						<div class="form-group d-flex justify-content-end">
+							<button type="submit" class="btn btn-primary">Kaydet</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup>
+import { useDefinitionStore } from '@/stores/admin.group.definition';
+import { ref } from 'vue';
+import router from '@/router';
+const name = ref("");
+const storeDefinition = useDefinitionStore();
+
+const submit = async () => {
+
+	await storeDefinition.addDefinition(
+		{
+			name: name.value,
+			code: getGroupCode()
+		});
+
+	name.value = "";
+
+	router.push({ path: '/admin/tanimlar/genel-tanimlar/grup-kodu-ve-adi-tanimlari/grup-kodu-ve-adi-tanimlari' })
+}
+
+const getCountTheName = () => {
+	const char = storeDefinition.getFirstCharactersCount.find(s => s.char == name.value.toString()[0]?.toUpperCase())
+	if (char) return char.count;
+	else return 0;
+}
+
+const getGroupCode = () => {
+	return (getCountTheName() + 1).toString().padStart(3, '0') + (name.value.toString().toUpperCase()[0] ?? 'X');
+}
+
+</script>
+<script >
+import { storeToRefs } from 'pinia';
+
+export default {
+	setup() {
+		const storeDefinition = useDefinitionStore();
+		const { definitions } = storeToRefs(storeDefinition);
+		return { definitions };
+	},
+	data() {
+		return ({
+			groupName: "",
+		})
+	},
+	methods: {
+		groupCode: function () {
+			return this.groupName ? '1' + this.groupName[0].toUpperCase() : '???';
+		}
+	},
+	computed: {
+		submit: function () {
+
+		}
+	}
+}
+</script>
---
+++ src/views/admin/tanimlar/GenelTanimlar.GrupKoduVeAdiTanimlari.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<div class="card-header">
+					<div class="row">
+						<div class="col">
+							Grup Kodları ve Adları
+						</div>
+						<div class="col-1">
+							<RouterLink class="btn btn-primary"
+								:to="{ name: 'admin.tanimlar.genel-tanimlar.grup-kodu-ve-adi-tanimlari.create' }">
+								Yeni +
+							</RouterLink>
+						</div>
+					</div>
+				</div>
+				<div class="card-body">
+					<table class="table table-hover">
+						<thead class="thead-dark">
+							<tr>
+								<th scope="col">Grup Kodu</th>
+								<th scope="col">Grup Adı</th>
+								<th scope="col-2">İşlemler</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="definition in storeDefinition.definitions">
+								<td>{{ definition.code }}</td>
+								<td>{{ definition.name }}</td>
+								<td>
+									<button class="btn btn-warning">Düzenle</button> |
+									<button class="btn btn-danger">Sil</button>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup>
+
+import { useDefinitionStore } from '@/stores/admin.group.definition';
+
+const storeDefinition = useDefinitionStore();
+
+</script>
+<style scoped>
+table.table td:last-child,
+table.table th:last-child {
+	display: flex;
+	justify-content: end;
+}
+</style>
